a0865a82e14b0aafa467fcf962742165fbdb3aee
jacobjohn90 <jacobjohn90@gmail.com>
2018-05-21 17:27:26 -0400

    made random number generator up and running

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log('hello')
+
+// Global Array's and variables
+let aiArray = []
+let userArray = []
+let gameRunning = false
+let id
+let color
+// Computer input
+$(document).ready(function () {
+    $('.startButton').click(function () {
+        if (gameRunning == false) {
+            // gameRunning = true
+            startGame()
+            console.log('start clicked')
+            let i = 0
+            color = $()
+            changeColor()
+           
+        }
+    })
+
+})
+
+// Random Number Generator
+let startGame = function () {
+    let randomNumber = Math.floor(Math.random() * 4)
+    aiArray.push(randomNumber)
+    console.log(aiArray)
+}
+
+// Change color displayed
+let changeColor = function () {
+
+}
+
+// Player Input
