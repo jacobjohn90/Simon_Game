c79343dc37b599bd55530dcc7f7f6830229c8b60
jacobjohn90 <jacobjohn90@gmail.com>
2018-05-22 11:33:25 -0400

    fixed color changing error

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     $('.startButton').click(function () {
         if (gameRunning == false) {
             // gameRunning = true
-            startGame()
+            startGame() // star game function
             // console.log('start clicked')
 
 
@@ -26,21 +26,32 @@ let startGame = function () {
     let randomNumber = Math.floor(Math.random() * 4)
     aiArray.push(randomNumber)
     console.log(aiArray)
-    let interval = setInterval(function () {
-        for (i = 0; i <= aiArray.length; i++) {
-            if (i == aiArray.length) {
-                clearInterval(interval)
-                return
-            } else {
-                id = aiArray[i]
-                color = $('#' + id).attr('class')
-                changeColor(color)
-            }
-        }
+
+    // loop through aiArray
+    // for each item, wait 1000
+    for (let i = 0; i <= aiArray.length; i++) {
+        setTimeout(function () {
+            id = aiArray[i]
+            color = $('#' + id).attr('class') //method found on stackoverflow
+            // console.log(color)
+            changeColor(color)
+        }, 1000 * (i + 1))
+    }
+    // let interval = setInterval(function () {
+    //     for (i = 0; i <= aiArray.length; i++) {
+    //         if (i == aiArray.length) {
+    //             clearInterval(interval)
+    //             return
+    //         } else {
+    //             id = aiArray[i]
+    //             color = $('#' + id).attr('class').split(" ")[0] //method found on stackoverflow
+    //             changeColor(color)
+    //         }
+    //     }
 
 
-    }, 1000)
-    
+    // }, 1000)
+    // userClick()
 }
 
 // Change color displayed
@@ -78,3 +89,34 @@ let changeColor = function (color) {
 }
 
 // Player Input
+// let picColor = 
+let userClick = function () {
+    $('.button').click(function () {
+        id = $(this).attr('id') //similar code to the one found on stackoverflow
+        userArray.push(Number(id))
+        console.log('user input is ' + userArray)
+        if (userArray.length == aiArray.length) {
+            arrayChecker()
+            // return
+        }
+    })
+}
+
+// Checking if array's are the same
+
+let arrayChecker = function () {
+    for (i = 0; i < aiArray.length; i++) {
+        if (aiArray[i] == userArray[i]) {
+            startGame()
+            // return
+        } if (aiArray[i] !== userArray[i]) {
+            gameOver()
+        }
+    }
+}
+
+// GAMEOVER
+
+let gameOver = function () {
+    console.log('incorrect')
+}
